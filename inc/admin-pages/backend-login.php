4e424f06cdd344af716386053cd7054dd41b2493
Jeremías San Martín <=>
2024-05-27 17:31:44 -0400

    sincronizo

--- inc/admin-pages/backend-login.php
+++ inc/admin-pages/backend-login.php
@@ -12,40 +12,4 @@ if ( ! $api_key ) {
 	echo '<div id="cfact_login_popup"></div>';
 }
 
-
-if ( isset( $_GET['set-api_key'] ) && isset( $_GET['api-key'] ) && ! empty( $_GET['api-key'] ) ) {
-
-
-	$api_key_data    = sanitize_text_field( wp_unslash( $_GET['api-key'] ) );
-	$keepme_informed = isset( $_GET['keepme-informed'] ) ? sanitize_text_field( wp_unslash( $_GET['keepme-informed'] ) ) : 'false';
-
-	if ( update_option( 'cfact_keepme_informed', $keepme_informed ) ) {
-		add_option( 'cfact_keepme_informed', $keepme_informed );
-	}
-
-	cfact_ld_api_key_mannger( 'add', $api_key_data );
-
-	cfact_integration_send_stadistic( 'set-apikey' );
-
-	$url  = 'https://coursefactory.reevolutiva.cl/?fluentcrm=1&route=contact&hash=34cf89d2-797e-4174-ba3d-411b75ff24e6';
-	$args = cfact_get_client_data();
-
-	if ( $keepme_informed ) {
-		$request = wp_remote_post( $url, $args );
-		$body    = wp_remote_retrieve_body( $request );
-		$body    = json_decode( $body );
-
-		#TODO: (AIDER) redireccion con php.
-		if ( $body->success ) :
-
-			?>
-			<script>
-				location.href = `${location.origin}/wp-admin/admin.php?page=course_factory_integration`
-			</script>
-			<?php
-
-		endif;
-	}
-}
-
 echo '<div id="cfact_login"></div>';
--- inc/admin-pages/backend.php
+++ inc/admin-pages/backend.php
@@ -8,21 +8,32 @@
 
 if ( ! defined( 'ABSPATH' ) ) exit;
 
+$api_key = cfact_ld_api_key_mannger( 'get' );
+
 $backend_i18n = cfact_backend_i18n();
 
-wp_localize_script( 'cfact-learndash-integration', 'bakendi18n', $backend_i18n );
+function bloginfo_array() {
+	$fields = array( 'name', 'description', 'url', 'admin_email', 'language' );
+	$data   = array();
+	foreach ( $fields as $field ) {
+		$data[ $field ] = get_bloginfo( $field );
+	}
+	return $data;
+}
 
-// Seccion donde se elimina el APIKEY de course factory.
 
-#TODO: (AIDER/) - "Fix Here".
-add_action('admin_init', function() {
-    if ( isset( $_GET['delete-api_key'] ) ) {
-        #cfact_ld_api_key_mannger( 'delete' );
-        wp_redirect( admin_url( 'admin.php?page=course_factory_integration' ) );
-        exit;
-    }
-});
-#(/AIDER)
+
+wp_add_inline_script( 'cfact-learndash-integration', 'var bakendi18n =' . wp_json_encode ( $backend_i18n ) . ";" , 'before' );
+wp_add_inline_script( 'cfact-learndash-integration', 'var req_project_list = false' . ";" , 'before' );
+wp_add_inline_script( 'cfact-learndash-integration', 'var cfact_blog_info ='. wp_json_encode( bloginfo_array()) . ";" ,'before' );
+wp_add_inline_script( 'cfact-learndash-integration', 'var cfact_current_user =' . wp_json_encode( wp_get_current_user() )  . ";" ,'before' );
+wp_add_inline_script( 'cfact-learndash-integration', 'var CFACT_PLUGIN_URL_COURSE_LOG="' . plugins_url( 'coursefactory-integration/course-log.json') . '";' , 'before' );
+if( $api_key === false ){
+	wp_add_inline_script( 'cfact-learndash-integration', 'var cfact_learndash_integration_apiKey = "";' ,'before' );	
+}else{
+	wp_add_inline_script( 'cfact-learndash-integration', 'var cfact_learndash_integration_apiKey ="'. $api_key . '";' ,'before' );	
+}
+
 
 // Seccion de listing.
 if ( $api_key && ! isset( $_GET['cfact_view_config'] ) ) {
@@ -31,7 +42,7 @@ if ( $api_key && ! isset( $_GET['cfact_view_config'] ) ) {
 
 
 // Seccion para logearse en course-factory.
-if ( ! isset($api_key) || isset( $_GET['cfact_view_config'] ) ) {
+if ( !$api_key || isset( $_GET['cfact_view_config'] ) ) {
 	require 'backend-login.php';
 }
 
