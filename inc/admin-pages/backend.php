ac3277d52ff9d58a1dea8ef041ddb42f9a7cf93e
= <=>
2024-05-27 15:05:07 -0400

    Added a TODO comment for fixing the API key deletion section.

--- inc/admin-pages/backend.php
+++ inc/admin-pages/backend.php
@@ -13,13 +13,15 @@ $backend_i18n = cfact_backend_i18n();
 wp_localize_script( 'cfact-learndash-integration', 'bakendi18n', $backend_i18n );
 
 // Seccion donde se elimina el APIKEY de course factory.
+
+#TODO: (AIDER/) - "Fix Here".
 if ( isset( $_GET['delete-api_key'] ) ) {
 	#cfact_ld_api_key_mannger( 'delete' );
 	wp_redirect( admin_url( 'admin.php?page=course_factory_integration' ) );
 	exit;
 
-	//TODO: (AIDER/)
 }
+#(/AIDER)
 
 // Seccion de listing.
 if ( $api_key && ! isset( $_GET['cfact_view_config'] ) ) {
