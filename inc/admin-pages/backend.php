6e6d5639a0833e2c1c67411b31ed90f61e4cbc4f
= <=>
2024-05-27 14:19:03 -0400

    added_localization_for_learndash_integration_script

--- inc/admin-pages/backend.php
+++ inc/admin-pages/backend.php
@@ -8,12 +8,13 @@
 
 if ( ! defined( 'ABSPATH' ) ) exit;
 
-wp_enqueue_script( 'cfact-backend-script', plugins_url( 'coursefactory-integration/js/backend.js', __FILE__ ), array( 'jquery' ), '1.0.0', true );
-wp_localize_script( 'cfact-backend-script', 'cfact_backend_i18n', $backend_i18n );
+$backend_i18n = cfact_backend_i18n();
+
+wp_localize_script( 'cfact-learndash-integration', 'bakendi18n', $backend_i18n );
 
 // Seccion donde se elimina el APIKEY de course factory.
 if ( isset( $_GET['delete-api_key'] ) ) {
-	cfact_ld_api_key_mannger( 'delete' );
+	#cfact_ld_api_key_mannger( 'delete' );
 	wp_redirect( admin_url( 'admin.php?page=course_factory_integration' ) );
 	exit;
 
