0df0f36d558d87aae544612e687be431eb3c28b7
= <=>
2024-05-27 13:57:12 -0400

    Added backend-listing.php and backend-login.login.php files to admin-pages/backend.php.

--- inc/admin-pages/backend.php
+++ inc/admin-pages/backend.php
@@ -1,12 +1,12 @@
 <?php
 /**
- *  Path: wp-content/plugins/coursefactory-integration/inc/backend.php.
+ *  Path: wp-content/plugins/coursefactory-integration/inc/admin-pages/backend.php.
  *  Este archivo contiene el codigo de la pagina de admistracion de coursefactory.
  *
  * @package Course Factory Integration
  */
 
-if ( ! defined( 'ABSPATH' ) ) exit; 
+if ( ! defined( 'ABSPATH' ) ) exit;
 
 wp_enqueue_script( 'cfact-backend-script', plugins_url( 'coursefactory-integration/js/backend.js', __FILE__ ), array( 'jquery' ), '1.0.0', true );
 wp_localize_script( 'cfact-backend-script', 'cfact_backend_i18n', $backend_i18n );
@@ -16,15 +16,14 @@ if ( isset( $_GET['delete-api_key'] ) ) {
 
 	cfact_ld_api_key_mannger( 'delete' );
 	?>
-	
-	#TODO: (AIDER/) redirecciona la url que defino en la linea de abajo usando la funcion wp_redirect.
-	
 	<script>
 		location.href = `${location.origin}/wp-admin/admin.php?page=course_factory_integration`;
 	</script>
 	<?php
 	die();
-	
+
+	#TODO: (AIDER/) redirecciona la url que defino en la linea de abajo usando la funcion wp_redirect.
+
 	#(/AIDER)
 
 }
@@ -32,10 +31,3 @@ if ( isset( $_GET['delete-api_key'] ) ) {
 // Seccion de listing.
 if ( $api_key && ! isset( $_GET['cfact_view_config'] ) ) {
 	require 'backend-listing.php';
-}
-
-// Seccion para logearse en course-factory.
-if ( ! $api_key || isset( $_GET['cfact_view_config'] ) ) {
-	require 'backend-login.login.php';
-}
-
