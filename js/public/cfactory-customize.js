ec6140916d1885278a547f2e48a2da5fcbba9e0b
Jeremías San Martín <=>
2024-05-27 12:39:46 -0400

    elimino js desde php

--- js/public/cfactory-customize.js
+++ js/public/cfactory-customize.js
@@ -5,3 +5,21 @@ if(location.href.includes("topics")){
     }
     
 }    
+
+if( document.querySelectorAll(".ld-item-list-items .ld-item-name .ld-item-components > span") ){
+
+    const cfact_ld_topic_itemo_for_delete = document.querySelectorAll(".ld-item-list-items .ld-item-name .ld-item-components > span");
+
+for (let index = 0; index < cfact_ld_topic_itemo_for_delete.length; index++) {
+
+    const element = cfact_ld_topic_itemo_for_delete[index];
+
+    if( element.textContent.includes("Topics") ){
+        element.style.display = "none";
+        cfact_ld_topic_itemo_for_delete[index - 1].style.display = "none";
+    }
+    
+}
+
+
+}
