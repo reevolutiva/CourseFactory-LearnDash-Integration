26df7a90f1be351ecc1b5d0129242b2d207ba439
= <=>
2024-05-28 13:15:34 -0400

    Improved the caching mechanism for retrieving course post IDs in the course importer.

--- inc/course-importer.php
+++ inc/course-importer.php
@@ -98,12 +98,17 @@ function cfact_ld_course_create( $title, $description, $outcome_list, $structure
 		$post_id = wp_cache_get( 'cfact_course_post_id_' . $id, 'cfact' );
 
 		if ( false === $post_id ) {
-			$post_id = $wpdb->get_var(
-				$wpdb->prepare(
-					'SELECT post_id FROM `wp_postmeta` WHERE `meta_key` = "cfact_project_version_id" AND `meta_value` = %s LIMIT 1',
-					$id
-				)
-			);
+			$post_id = wp_cache_get( 'cfact_course_post_id_' . $id, 'cfact' );
+
+			if ( false === $post_id ) {
+				$post_id = $wpdb->get_var(
+					$wpdb->prepare(
+						'SELECT post_id FROM `wp_postmeta` WHERE `meta_key` = "cfact_project_version_id" AND `meta_value` = %s LIMIT 1',
+						$id
+					)
+				);
+				wp_cache_set( 'cfact_course_post_id_' . $id, $post_id, 'cfact' );
+			}
 			wp_cache_set( 'cfact_course_post_id_' . $id, $post_id, 'cfact' );
 		}
 		wp_cache_set( 'cfact_course_post_id_' . $id, $post_id, 'cfact' );
