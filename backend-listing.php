c3029a06b63be1912bbc0b7a2afb790fa946508d
= <=>
2024-05-28 14:01:33 -0400

    (no commit message provided)

--- backend-listing.php
+++ backend-listing.php
@@ -1,12 +1,18 @@
 <?php
 /**
  * Backend listing page
+ *
+ * @package Course Factory Integration
  */
 
 // ... rest of code ...
 
 $table_name = $wpdb->prefix . 'coursefac_listings';
-$query      = "SELECT * FROM {$table_name}";
-$listings   = $wpdb->get_results( $query );
+$query      = $wpdb->prepare( "SELECT * FROM {$table_name} WHERE 1=1" );
+$listings   = wp_cache_get( 'coursefac_listings', 'coursefac' );
+if ( false === $listings ) {
+    $listings = $wpdb->get_results( $query );
+    wp_cache_set( 'coursefac_listings', $listings, 'coursefac' );
+}
 
 // ... rest of code ...
