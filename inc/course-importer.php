d5c60c6b20429b64b391492dce6879851faf3b52
= <=>
2024-05-28 12:22:48 -0400

    Updated course importer function to accept course ID and user ID parameters.

--- inc/course-importer.php
+++ inc/course-importer.php
@@ -3,8 +3,10 @@
  * Esta funcion se encarga de crear un curso en LearnDash. Hace una insercion en
  * la tabla wp_posts con el post_type sfwd-courses y crea un custom field con el
  * id del proyecto en CourseFactory. El post-content tendra un custom field con el
+ * id del proyecto en CourseFactory. El post-content tendra un custom field con el
  * id del proyecto en CourseFactory.
- * 
- * @param array $course_importer_data Datos del curso
- * @return string
+ *
+ * @param string $course_id
+ * @param string $user_id
+ * @return void
  */
