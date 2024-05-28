c7f43213ab0f79eeb976c5c778bbc11abce21827
= <=>
2024-05-28 13:26:00 -0400

    Adjusted the SQL query in the course deleter to use the correct table name.

--- inc/course-deleter.php
+++ inc/course-deleter.php
@@ -35,7 +35,7 @@ function coursefac_delete_course( $course_id ) {
 		$cpt_id_array = $wpdb->get_results(
 			$wpdb->prepare(
 				"SELECT post_id 
-				FROM $table_name
+				FROM {$wpdb->postmeta}
 				WHERE meta_key = 'course_id' 
 					AND meta_value = %d",
 				$course_id
